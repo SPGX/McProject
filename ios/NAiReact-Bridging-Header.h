c8c78ba970e0876af8570de3b2db497b63af69ba
Supagorn Srichan <41179747+SPGX@users.noreply.github.com>
2023-01-27 16:29:57 +0700

    initial commit

---
+++ ios/NAiReact-Bridging-Header.h
@@ -0,0 +1,8 @@
+// include ccidlib
+#import "NAiReact/CCIDLib/include/winscard.h"
+#import "NAiReact/CCIDLib/include/ReaderInterface.h"
+#import "NAiReact/CCIDLib/include/ft301u.h"
+// include nios lib
+#include "NAiReact/NIOSLib/include/NiOS.h"
+
+#import "React/RCTBridgeModule.h"
